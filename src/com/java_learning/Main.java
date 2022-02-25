2031f0cc0e804c227409c393fc70975b89976f4f
Гргорий Рак <g.s.rak@yandex.ru>
2022-02-25 23:28:29 +0300

    HW_1 add

--- src/com/java_learning/Main.java
+++
@@ -1,8 +0,0 @@
-package com.java_learning;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
---
+++ src/com/java_learning/lesson_1/HomeWorkApp.java
@@ -0,0 +1,52 @@
+package com.java_learning.lesson_1;
+
+public class HomeWorkApp {
+
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    public static void  printThreeWords(){
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign(){
+        int a = 5;
+        int b = -10;
+        if (a + b >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+//    В случае, когда значение более 100 описывать как "else if (value > 100)" смысла не имеет, т.к.
+//    мы уже имеем полный спект (диапазон) описания значений.
+    public static void printColor(){
+        int value = 101;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100){
+            System.out.println("Жёлтый");
+        } else {
+            System.out.println("Зелёный");
+        }
+    }
+
+    public static void compareNumbers(){
+        int a = 10;
+        int b = 5;
+
+        if (a >= b){
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+
+}
