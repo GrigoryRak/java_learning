41b06c330194f2d35cd04eba925244786acbb911
Гргорий Рак <g.s.rak@yandex.ru>
2022-03-01 16:26:12 +0300

    HW_2 add

---
+++ src/com/java_learning/lesson_2/HomeWorkMethod.java
@@ -0,0 +1,52 @@
+package com.java_learning.lesson_2;
+
+import java.util.*;
+
+public class HomeWorkMethod {
+    public static void main(String[] args) {
+
+//        Date date = new Date();
+
+        System.out.println("Сумма чисел a и b лежит в диапазоне от 10 до 20 (включительно) - " + checkSum(5,10));
+        System.out.println();
+        System.out.println(numberCheck(-5));
+        System.out.println();
+        System.out.println("Верно ли, что введено отрицательное число? Результат: " + numberCheckBool(-1));
+        System.out.println();
+        cycle("Проход по циклу - ", 7);
+        System.out.println();
+        System.out.println(definitionLeapYear(2021));
+    }
+
+    public static boolean checkSum(int a, int b){
+        int summa = a + b;
+        return summa >= 10 && summa <= 20;
+    }
+
+    public static String numberCheck(int a){
+        String numberCheckResult;
+        if (a >=0) {
+            numberCheckResult = " - является числом положительным;";
+        } else {
+            numberCheckResult = " - является числом отрицательным;";
+        }
+        return a + numberCheckResult;
+    }
+
+    public static boolean numberCheckBool(int a){
+        return a < 0;
+    }
+
+    public static String cycle(String a, int b){
+        for(int i = 1; i <= b; i++){
+            System.out.println(a + i);
+        }
+        return a;
+    }
+
+    public static boolean definitionLeapYear(int year){
+        GregorianCalendar  leapYear = new GregorianCalendar();
+        return leapYear.isLeapYear(year);
+    }
+
+}
